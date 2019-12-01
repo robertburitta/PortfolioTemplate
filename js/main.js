9fbcf4d7ac1ec9719b13a0477a3af8a3c4f6f685
robcioman <robertburitta@gmail.com>
2019-12-01 22:17:31 +0100

    Fifth commit - propably final

--- js/main.js
+++ js/main.js
@@ -22,4 +22,37 @@ $(document).ready(function(){
         gallery: {enabled: true}
     });
 
+    //Owl Carousel
+    $('.site-main .about-area .owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            544: {
+                items: 2
+            }
+        }
+    })
+
+    //Sticky Navigation Menu
+    let nav_offset_top = $('.header_area').height() + 50;
+
+    function navbarFixed() {
+        if ($('.header_area').length) {
+            $(window).scroll(function() {
+                let scroll = $(window).scrollTop();
+                if (scroll >= nav_offset_top) {
+                    $('.header_area .main-menu').addClass('navbar_fixed');
+                } else {
+                    $('.header_area .main-menu').removeClass('navbar_fixed');
+                }
+            })
+        }
+    }
+
+    navbarFixed();
+
 });
